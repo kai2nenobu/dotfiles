2510b73c184e9c852ff2d792569d215e9746fca6
Tsunenobu Kai <kai2nenobu@gmail.com>
2018-07-20 09:37:56 +0900

    プロキシが必要なケースを検知する

--- win_setup/setup.ps1
+++ win_setup/setup.ps1
@@ -1,10 +1,23 @@
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$ExitCode = @{
+  Success = 0
+  FailToInstallChocolatey = 1
+}
 
 $chocolateyUrl = 'https://chocolatey.org/install.ps1'
+$installScript = 'install.ps1'
 
+# Download chocolatey install script
 try {
-  $res = Invoke-WebRequest -Uri $chocolateyUrl
+  $res = Invoke-WebRequest -Uri $chocolateyUrl -OutFile $installScript
 } catch [System.Net.WebException] {
   $ex = $_.Exception
-  $ex | ConvertTo-Json
-  Write-Error 'Fail to fetch a chocolatey install script.'
+  #$ex | ConvertTo-Json
+  if ($ex.Response.StatusCode -eq 407) {
+    Write-Error 'Proxy configuration is required.'
+  } else {
+    Write-Error 'Fail to fetch a chocolatey install script.'
+    exit $ExitCode.FailToInstallChocolatey
+  }
 }
