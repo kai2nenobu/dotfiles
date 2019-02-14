dc24c2e6fb958781e51b8eb744f2418c34e414b6
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-02-14 12:38:47 +0900

    Ansible PlaybookをWSLで実行するバッチスクリプトを追加する
    ポチッとダブルクリックするだけで、Playbookを適用できるよ。

---
+++ ansible/WslAnsible.bat
@@ -0,0 +1,18 @@
+@echo off
+
+REM
+REM Ansible PlaybookをWSLで実行する
+REM
+REM 前提条件
+REM - Windows 10 Pro以上
+REM - バージョン1803以上
+
+set HERE=%~dp0
+
+REM wsl.exe を使う場合（既定のディストリで実行するよ）
+wsl.exe bash -c "cd $(wslpath -ua '%HERE%'); ansible-playbook -vvv -i hosts config.yml"
+
+REM LxRunOffline を使う場合（-n で指定したディストリで実行するよ）
+REM LxRunOffline.exe run -n Ubuntu-18.04 -c "cd $(wslpath -ua '%USERPROFILE%\repo\setupper\ansible'); ansible-playbook -vvv -i hosts config.yml"
+
+set /p=ENTERで閉じます...
