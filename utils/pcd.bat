42fb7d6e2eb1a23d0c32b3cad22c27d8f46c7cdd
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-03-06 12:39:39 +0900

    あらかじめ登録しておいたディレクトリにcdするバッチを追加する
    https://mattn.kaoriya.net/software/peco.htm
    をまるまる使わせてもらった。

---
+++ utils/pcd.bat
@@ -0,0 +1,41 @@
+@echo off
+
+if "%1" equ "add" (
+  if "%2" neq "" (
+    echo %2 >> "%USERPROFILE%\.peco-cd"
+    goto end
+  )
+  goto usage
+)
+if "%1" equ "edit" (
+  goto edit
+)
+if "%1" neq "" (
+  goto usage
+)
+goto query
+
+:usage
+echo %0 [add PATH ^| edit]
+goto end
+
+:edit
+if "%EDITOR%" neq "" (
+  "%EDITOR%" "%USERPROFILE%\.peco-cd"
+  goto end
+)
+notepad "%USERPROFILE%\.peco-cd"
+goto end
+
+:query
+rem NOTE:
+rem
+rem If you have a problem caused by character-set, modify below part like:
+rem   'type ^"%USERPROFILE%\.peco-cd^" ^| iconv -f char -t utf-8 ^| peco'
+rem
+for /f %%i in ('type ^"%USERPROFILE%\.peco-cd^" ^| peco') do (
+  cd %%i
+  break
+)
+
+:end
