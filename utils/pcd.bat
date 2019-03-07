35a8783190276ea20ad163690d7809e77565dc39
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-03-07 12:42:54 +0900

    pecoの代わりにfzfを使ってみることにした

--- utils/pcd.bat
+++ utils/pcd.bat
@@ -2,7 +2,7 @@
 
 if "%1" equ "add" (
   if "%~2" neq "" (
-    echo %~2>> "%USERPROFILE%\.peco-cd"
+    echo %~2>> "%USERPROFILE%\.fzf-cd"
     goto end
   )
   goto usage
@@ -21,19 +21,19 @@ goto end
 
 :edit
 if "%EDITOR%" neq "" (
-  "%EDITOR%" "%USERPROFILE%\.peco-cd"
+  "%EDITOR%" "%USERPROFILE%\.fzf-cd"
   goto end
 )
-notepad "%USERPROFILE%\.peco-cd"
+notepad "%USERPROFILE%\.fzf-cd"
 goto end
 
 :query
 rem NOTE:
 rem
 rem If you have a problem caused by character-set, modify below part like:
-rem   'type ^"%USERPROFILE%\.peco-cd^" ^| iconv -f char -t utf-8 ^| peco'
+rem   'type ^"%USERPROFILE%\.fzf-cd^" ^| iconv -f char -t utf-8 ^| fzf'
 rem
-for /f "delims=" %%i in ('type ^"%USERPROFILE%\.peco-cd^" ^| iconv -f cp932 -t utf-8 ^| peco ^| iconv -f utf-8 -t cp932') do (
+for /f "delims=" %%i in ('type ^"%USERPROFILE%\.fzf-cd^" ^| iconv -f cp932 -t utf-8 ^| fzf ^| iconv -f utf-8 -t cp932') do (
   cd /d "%%i"
   break
 )
