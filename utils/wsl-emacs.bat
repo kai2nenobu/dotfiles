4c350307c2feae89d87e1bc838392440403c0b0d
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-03-19 13:39:07 +0900

    wslのEmacsを起動するバッチファイルを追加する
    デフォルトのWSLディストリでEmacsを起動できる。Xサーバが立ち上がっている前提です。

---
+++ utils/wsl-emacs.bat
@@ -0,0 +1,11 @@
+@if (@X)==(@Y) @end /* Harmless hybrid line that begins a JScript comment
+
+::--- Batch section within JScript comment that calls the internal JScript ----
+@echo off
+cscript //E:JScript //nologo "%~f0" %*
+exit /b
+
+----- End of JScript comment, beginning of normal JScript  ------------------*/
+var ws = new ActiveXObject("Wscript.Shell");
+// Launch emacs in WSL
+ws.run('wsl bash -c "export DISPLAY=0:0 && exec emacs -r"', 0, False);
