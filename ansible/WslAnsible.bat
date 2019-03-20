8913c57bd3c90726e027fcf1891c9ce3ac5c9814
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-03-20 11:12:52 +0900

    エントリーポイントをsite.ymlに変更する

--- ansible/WslAnsible.bat
+++ ansible/WslAnsible.bat
@@ -9,10 +9,8 @@ REM - バージョン1803以上
 
 set HERE=%~dp0
 
-wslconfig /s "Debian"
 REM wsl.exe を使う場合（既定のディストリで実行するよ）
-wsl.exe bash -c "cd $(wslpath -ua '%HERE%'); ansible-playbook -vvv -i hosts config.yml %*"
-wslconfig /s "Ubuntu-18.04"
+wsl.exe bash -c "cd $(wslpath -ua '%HERE%'); ansible-playbook -vvv -i hosts site.yml %*"
 
 REM LxRunOffline を使う場合（-n で指定したディストリで実行するよ）
 REM LxRunOffline.exe run -n Ubuntu-18.04 -c "cd $(wslpath -ua '%USERPROFILE%\repo\setupper\ansible'); ansible-playbook -vvv -i hosts config.yml"
