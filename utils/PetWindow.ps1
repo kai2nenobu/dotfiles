a6b71d28ddd574c0edb5892d74fde9656630de76
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-06-20 17:46:00 +0900

    petによるスニペット選択スクリプトをpowershellで書き直した
    powershellのほうが鬼のように簡単だった。powershellのほうが若干
    起動が遅くなるけど気になるほどではない。

---
+++ utils/PetWindow.ps1
@@ -0,0 +1,13 @@
+﻿<#
+  .SYNOPSIS
+    Show a window to select a snippet by pet and insert it.
+#>
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+$snip = pet search
+if ([String]::isNullOrEmpty($snip)) {
+  exit 0
+}
+$quotedSnip = $snip -replace '"','""'  # escape double quotes
+AutoHotkey.exe (Join-Path $PSScriptRoot "insert_text.ahk") "$quotedSnip"
