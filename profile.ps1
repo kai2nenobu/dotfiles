08ac274fdf29e5baf7d7effa9f38490e2d4a7226
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-09-13 17:07:12 +0900

    git、docker関連のエイリアスを追加する
    powershellではエイリアスには単独のコマンドしか指定できない。
    引数を伴うコマンドは定義できないので、関数を定義することで
    エイリアスの代わりにしている。
    
    コメントアウトしているのはすでにエイリアスが存在するために、
    定義できないものである。

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,51 @@
 Set-PSReadLineOption -EditMode Emacs
 
+<# Alias configuration #>
+# git
+Set-Alias g git
+function gs() {
+  Start-Process git -ArgumentList status,--short,--branch -NoNewWindow -Wait
+}
+function gst() {
+  Start-Process git -ArgumentList stash -NoNewWindow -Wait
+}
+#function gl() {
+#  Start-Process git -ArgumentList log -NoNewWindow -Wait
+#}
+function gla() {
+  Start-Process git -ArgumentList log,--decorate,--graph,--all -NoNewWindow -Wait
+}
+function glo() {
+  Start-Process git -ArgumentList log,--decorate,--graph,--all,--oneline -NoNewWindow -Wait
+}
+function gd() {
+  Start-Process git -ArgumentList diff,--histogram -NoNewWindow -Wait
+}
+#function gc() {
+#  Start-Process git -ArgumentList commit -NoNewWindow -Wait
+#}
+function ga() {
+  Start-Process git -ArgumentList add -NoNewWindow -Wait
+}
+function gco() {
+  Start-Process git -ArgumentList checkout -NoNewWindow -Wait
+}
+function gb() {
+  Start-Process git -ArgumentList branch -NoNewWindow -Wait
+}
+#function gp() {
+#  Start-Process git -ArgumentList pull -NoNewWindow -Wait
+#}
+function gn() {
+  Start-Process git -ArgumentList now,--all,--stat -NoNewWindow -Wait
+}
+function gam() {
+  Start-Process git -ArgumentList commmit,--amend,--no-edit -NoNewWindow -Wait
+}
+
+# docker
+Set-Alias d docker
+Set-Alias dc docker-compose
+Set-Alias dm docker-machine
+
 "Read a profile from `"$profile`""
