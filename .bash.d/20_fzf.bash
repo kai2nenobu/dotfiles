96c2743c9b684e79bd5812ffc64f533f8e0fcf7b
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-03-11 17:13:39 +0900

    内部的な関数であることがわかるように、接頭辞の _ を付与する

--- .bash.d/20_fzf.bash
+++ .bash.d/20_fzf.bash
@@ -2,23 +2,23 @@ if ! _find_command fzf; then
   _log "Not found 'fzf' in PATH" && return
 fi
 
-fzf_history() {
+_fzf_history() {
   declare l=$(HISTTIMEFORMAT=  history | tac |  awk '{for(i=2;i<NF;i++){printf("%s%s",$i,OFS=" ")}print $NF}' | fzf --query "$READLINE_LINE" --prompt 'Choose history: ' --no-sort -0 -1)
   READLINE_LINE="$l"
   READLINE_POINT=${#l}
 }
-bind -x '"\C-r": fzf_history'
+bind -x '"\C-r": _fzf_history'
 
-fzf_cd_recursive() {
+_fzf_cd_recursive() {
   cd "$(find -name '.git' -prune -o -type d -printf '%P\n' | fzf --prompt 'Choose directory: ' -0 -1)"
 }
-bind -x '"\C-x\C-d": fzf_cd_recursive'
+bind -x '"\C-x\C-d": _fzf_cd_recursive'
 
 if [ -n "$CMDER_USER_CONFIG" ]; then
-  fzf_cmder_history() {
+  _fzf_cmder_history() {
     declare l=$(cat "${CMDER_USER_CONFIG}/.history" | fzf --tac --query "$READLINE_LINE" --prompt 'Choose history: ' --no-sort -0 -1)
     READLINE_LINE="$l"
     READLINE_POINT=${#l}
   }
-  bind -x '"\C-x\C-r": fzf_cmder_history'
+  bind -x '"\C-x\C-r": _fzf_cmder_history'
 fi
