792f320b202f23496b3c9afffe95b3de6ce5b1d4
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-03-11 19:16:27 +0900

    Git組み込みのプロンプト関数を使用する
    Git組み込みの git-prompt.sh で定義されている __git_ps1 関数を
    使うとプロンプトに出すのにいい感じのブランチ情報が取得できる。
    
    Git Bashだと "C:\Program Files\Git\mingw64\share\git\completion\git-prompt.sh"
    Ubuntuなら "/usr/lib/git-core/git-sh-prompt" あたりに配置してある。
    上記の環境はbashを起動するだけ __git_ps1 関数が使えるが、MSYS2では
    そうはいかない。
    
    ということでGit Bashの git-prompt.sh を読み込むようにしてみた。
    Git Bashがインストールされてなければ、何もしない空実装になる。
    
    - https://seesaawiki.jp/w/kou1okada/d/git%20-%20git-prompt.sh

--- .bash.d/11_prompt.bash
+++ .bash.d/11_prompt.bash
@@ -7,4 +7,15 @@ function _prompt_exit_code() {
   fi
 }
 
-PS1='\[\033[01;32m\]\u@\h\[\033[00m\]\[\033[1;31m\]`_prompt_exit_code`\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ '
+if ! type __git_ps1 &> /dev/null; then
+  git_prompt='/c/Program Files/Git/etc/profile.d/git-prompt.sh'
+  if [ -f "$git_prompt" ]; then
+    . "$git_prompt"
+  else
+    function __git_ps1() {
+      : # 空実装
+    }
+  fi
+fi
+
+PS1='\[\033[01;32m\]\u@\h\[\033[00m\]\[\033[1;31m\]`_prompt_exit_code`\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[1;33m\]`__git_ps1`\[\033[00m\]\n\$ '
