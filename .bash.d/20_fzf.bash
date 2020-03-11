96ad177dffe85d1a384060b8cc44516f447b340e
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-03-11 17:16:43 +0900

    簡単に関数のドキュメントを書く

--- .bash.d/20_fzf.bash
+++ .bash.d/20_fzf.bash
@@ -2,6 +2,7 @@ if ! _find_command fzf; then
   _log "Not found 'fzf' in PATH" && return
 fi
 
+## 履歴を絞り込む
 _fzf_history() {
   declare l=$(HISTTIMEFORMAT=  history | tac |  awk '{for(i=2;i<NF;i++){printf("%s%s",$i,OFS=" ")}print $NF}' | fzf --query "$READLINE_LINE" --prompt 'Choose history: ' --no-sort -0 -1)
   READLINE_LINE="$l"
@@ -9,12 +10,14 @@ _fzf_history() {
 }
 bind -x '"\C-r": _fzf_history'
 
+## 作業ディレクトリの以下のディレクトリに移動する
 _fzf_cd_recursive() {
   cd "$(find -name '.git' -prune -o -type d -printf '%P\n' | fzf --prompt 'Choose directory: ' -0 -1)"
 }
 bind -x '"\C-x\C-d": _fzf_cd_recursive'
 
 if [ -n "$CMDER_USER_CONFIG" ]; then
+  ## Cmderの履歴を絞り込む
   _fzf_cmder_history() {
     declare l=$(cat "${CMDER_USER_CONFIG}/.history" | fzf --tac --query "$READLINE_LINE" --prompt 'Choose history: ' --no-sort -0 -1)
     READLINE_LINE="$l"
