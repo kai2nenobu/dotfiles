129fed82b5673b81a24df81eb1bac9d584d43a28
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-03-11 17:08:50 +0900

    fzfとの連携設定を分離させる

--- .bash.d/10_config.bash
+++ .bash.d/10_config.bash
@@ -102,31 +102,6 @@ if which peco &> /dev/null && [ -n "$ConEmuBuild" ]; then
   bind -x '"\C-x\C-d": peco_cd_recursive'
 fi
 
-## fzf integration
-if which fzf &> /dev/null && [ -n "$ConEmuBuild" ]; then
-  fzf_history() {
-    declare l=$(HISTTIMEFORMAT=  history | tac |  awk '{for(i=2;i<NF;i++){printf("%s%s",$i,OFS=" ")}print $NF}' | fzf --query "$READLINE_LINE" --prompt 'Choose history: ' --no-sort -0 -1)
-    READLINE_LINE="$l"
-    READLINE_POINT=${#l}
-  }
-  bind -x '"\C-r": fzf_history'
-
-  fzf_cd_recursive() {
-    cd "$(find -name '.git' -prune -o -type d -printf '%P\n' | fzf --prompt 'Choose directory: ' -0 -1)"
-  }
-  bind -x '"\C-x\C-d": fzf_cd_recursive'
-
-  if [ -n "$CMDER_USER_CONFIG" ]; then
-    fzf_cmder_history() {
-      declare l=$(cat "${CMDER_USER_CONFIG}/.history" | fzf --tac --query "$READLINE_LINE" --prompt 'Choose history: ' --no-sort -0 -1)
-      READLINE_LINE="$l"
-      READLINE_POINT=${#l}
-    }
-    bind -x '"\C-x\C-r": fzf_cmder_history'
-  fi
-
-fi
-
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="${HOME}/.sdkman"
 [[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
