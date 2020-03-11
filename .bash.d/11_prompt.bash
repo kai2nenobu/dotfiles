e78241998e0f2b031dc119a6ed077cc0e3fef7bb
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-03-11 19:39:57 +0900

    終了コードをプロンプトの後ろの方に移動する
    プロンプトで host:/path のように表示されたほうがわかりやすいかな、
    終了コードをということで /path の後ろに持っていくことにした。

--- .bash.d/11_prompt.bash
+++ .bash.d/11_prompt.bash
@@ -3,7 +3,7 @@ function _prompt_exit_code() {
   if [ $last_exit_code -eq 0 ]; then
     echo ''
   else
-    echo "<$last_exit_code>"
+    echo " <$last_exit_code>"
   fi
 }
 
@@ -21,12 +21,12 @@ fi
 PS1=''
 PS1="$PS1"'\[\033[01;32m\]'       # GREEN
 PS1="$PS1"'\u@\h'                 # user@host
-PS1="$PS1"'\[\033[1;31m\]'        # RED
-PS1="$PS1"'`_prompt_exit_code`'   # <exit code>
 PS1="$PS1"'\[\033[00m\]'          # RESET COLOR
 PS1="$PS1"':'                     # :
 PS1="$PS1"'\[\033[01;34m\]'       # BLUE
 PS1="$PS1"'\w'                    # current working directory
+PS1="$PS1"'\[\033[1;31m\]'        # RED
+PS1="$PS1"'`_prompt_exit_code`'   # <exit code>
 PS1="$PS1"'\[\033[1;33m\]'        # YELLOW
 PS1="$PS1"'`__git_ps1`'           # git info (if available)
 PS1="$PS1"'\[\033[00m\]'          # RESET COLOR
