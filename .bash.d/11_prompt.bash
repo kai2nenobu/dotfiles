f189e20e3aedc942d43e198e8208536cb73f914d
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-03-11 18:45:31 +0900

    終了コードが0以外のときにその終了コードをプロンプトに出す

--- .bash.d/11_prompt.bash
+++ .bash.d/11_prompt.bash
@@ -1,1 +1,10 @@
-PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ '
+function _prompt_exit_code() {
+  local last_exit_code=$?
+  if [ $last_exit_code -eq 0 ]; then
+    echo ''
+  else
+    echo "<$last_exit_code>"
+  fi
+}
+
+PS1='\[\033[01;32m\]\u@\h\[\033[00m\]\[\033[1;31m\]`_prompt_exit_code`\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\$ '
