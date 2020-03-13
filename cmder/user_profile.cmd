bbffdfb0fa4b6a2934298012567a25a308d6db8d
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-03-13 10:52:50 +0900

    コマンドプロンプトでもWindows Terminalの場合のみUTF-8にする

--- cmder/user_profile.cmd
+++ cmder/user_profile.cmd
@@ -11,6 +11,9 @@
 :: you can add your plugins to the cmder path like so
 :: set "PATH=%CMDER_ROOT%\vendor\whatever;%PATH%"
 
+:: Use UTF-8 in Windows Terminal
+if defined WT_SESSION chcp 65001
+
 echo Load user profile (%~f0)
 
 set MY_VARIABLE=cmder
