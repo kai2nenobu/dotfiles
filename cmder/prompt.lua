bc3ac848328fa1daf7c01a22ee5ac8b6ebecfcbe
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-03-19 19:35:50 +0900

    後続のフィルター関数を継続するために false を返す
    return false しておくと、後続のフィルター処理を継続する仕様である。
    明示的に値を返さない場合も継続しそうだけど、一応ね。
    
    ref. https://github.com/mridgers/clink/blob/298113a61272afa3f646a912c97e2ce34da67597/docs/clink.md#customising-the-prompt

--- cmder/prompt.lua
+++ cmder/prompt.lua
@@ -5,6 +5,7 @@ local function my_prompt_filter()
     local lambda = "λ"
     local my_prompt = ">"
     clink.prompt.value = string.gsub(clink.prompt.value, lambda, my_prompt)
+    return false
 end
 
 clink.prompt.register_filter(my_prompt_filter, 100)
