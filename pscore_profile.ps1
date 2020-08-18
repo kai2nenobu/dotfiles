18b5ff39c301c337045008ef0495923ec9ee16b6
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-18 11:46:33 +0900

    プロキシを有効、無効にする関数を定義する

--- pscore_profile.ps1
+++ pscore_profile.ps1
@@ -1,1 +1,16 @@
 Set-PSReadLineOption -EditMode Emacs
+
+function Enable-Proxy {
+  $url = 'http://localhost:8888'
+  $env:http_proxy = $url
+  $env:https_proxy = $url
+  $env:no_proxy = 'localhost'
+  $PSDefaultParameterValues = @{ "*:Proxy"=$url }
+}
+
+function Disable-Proxy {
+  $env:http_proxy = ''
+  $env:https_proxy = ''
+  $env:no_proxy = ''
+  $PSDefaultParameterValues = @{}
+}
