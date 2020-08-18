32b996c2026ddf2a1d3b410fa2a4c60d4e8e8b3c
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-18 15:03:40 +0900

    fzfとの連携設定を追加する
    PSFzfのロードに2秒程度かかるのパフォーマンス的に嫌な感じ。
    fzfコマンドのラップしかしてなさそうなので、PSFzfを使わない
    という手もありそう。
    
    が、めんどくさいのでとりあえず使う方針で行く。

--- pscore_profile.ps1
+++ pscore_profile.ps1
@@ -18,4 +18,16 @@ function Disable-Proxy {
   $PSDefaultParameterValues = @{}
 }
 
+function fzf_integration {
+  $psFzfModule = (Get-Module -ListAvailable PSFzf)
+  if (-not $psFzfModule) {
+    return # Nothing to do
+  }
+  "Use PSFzf ({0})" -f $psFzfModule.Path
+  Import-Module $psFzfModule
+  Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+}
+
+fzf_integration
+
 Set-StrictMode -Off
