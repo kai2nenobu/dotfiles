1a1d9cc05e8aefd2a3346a4d1560acf60ddbc348
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-28 02:39:49 +0900

    ansibleをブートストラップするスクリプトを追加する

---
+++ .bootstrap/ansible.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+### Bootstrap ansible with python3
+###
+### Prerequisite:
+###   OS: Ubuntu 18.04
+
+SUDO=sudo
+# Not use sudo if root
+[ "$(id -u)" = 0 ] && SUDO=
+
+$SUDO apt update
+$SUDO apt upgrade -y
+$SUDO apt install -y software-properties-common python3-pip
+$SUDO pip3 install ansible pywinrm
