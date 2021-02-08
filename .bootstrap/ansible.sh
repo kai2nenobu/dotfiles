c77e760e993af5f50625c0382fb9a7cdd2ccfb7d
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-02-08 16:20:28 +0900

    インストール時に対話プロンプトが出ないようにする

--- .bootstrap/ansible.sh
+++ .bootstrap/ansible.sh
@@ -3,12 +3,13 @@
 ### Bootstrap ansible with python3
 ###
 ### Prerequisite:
-###   OS: Ubuntu 18.04
+###   OS: Ubuntu 18.04 or later
 
 SUDO='sudo -E'
 # Not use sudo if root
 [ "$(id -u)" = 0 ] && SUDO=
 
 $SUDO apt update
-$SUDO apt install -y software-properties-common python3-pip python3-setuptools
+$SUDO DEBIAN_FRONTEND=noninteractive apt install -y \
+      software-properties-common python3-pip python3-setuptools
 $SUDO pip3 install ansible pywinrm
