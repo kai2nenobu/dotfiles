7050193f102a3e350efed76ccd2dd908889cfa43
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-02-13 17:32:21 +0900

    ansible実行用のvenvを作成してtar.gzに固めるスクリプトを追加する
    pipでansibleのインストールをするとものすごく時間がかかるので、
    すでに圧縮済みのvenvを直接ダウンロードして即座にansibleを
    使えるようにしたい。

---
+++ ci/create_ansible_venv.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+### Create a virtualenv to run ansible playbook by poetry and
+### archive it to share created virtualenv.
+
+set -eu
+
+DIR=$(dirname "$(readlink -e "$0")")
+
+## Install prerequisites
+sed -i.bak "s@http://archive.ubuntu.com/ubuntu/@mirror://mirrors.ubuntu.com/mirrors.txt@g" /etc/apt/sources.list
+apt update && apt install -y ca-certificates
+apt update && apt install -y curl python3 python3-dev python3-distutils
+
+## Install poetry
+export POETRY_HOME=/opt/poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+
+# shellcheck disable=SC1090
+. "${POETRY_HOME}/env"
+
+## Install dependent packages into venv
+cd ansible
+poetry install --no-dev
+
+## Move venv into a fixed location
+TARGET_LOCATION=/opt/ansible-venv
+venv_location=$(poetry env info -p)
+mv "$venv_location" "$TARGET_LOCATION"
+
+## Archive the venv
+. "/etc/os-release"
+cd "$TARGET_LOCATION"
+find . -name "*.pyc" -print0 | xargs -0 --no-run-if-empty -- rm
+grep "$venv_location" -RIl | xargs --no-run-if-empty -- sed -i "s@${venv_location}@${TARGET_LOCATION}@g"
+tar zcf "${DIR}/docs/${NAME}-${VERSION_ID}-ansible-venv.tar.gz" .
