46f767d00d4ec638be60ff04882775bcd768bbaa
kai2nenobu <kai2nenobu@users.noreply.github.com>
2021-03-18 09:17:06 +0000

    deploy: 0852466235f73db8e42d5ac86f3fcc030771bf3b

---
+++ README.md
@@ -0,0 +1,7 @@
+# dotfiles
+
+- [new.sh](./new.sh): Linux向け初期セットアップスクリプト
+
+```sh
+curl -sSL https://dotfiles.kaichan.info/new.sh | bash
+```
---
+++ new.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euC
+
+### Linux向け初期セットアップスクリプト
+
+
+### Functions
+fetch_dotfiles() {
+  dotfiels_url=https://github.com/kai2nenobu/dotfiles/archive/main.tar.gz
+  dotfiles_dir="$HOME/dotfiles"
+  mkdir -p "$dotfiles_dir"
+  curl -sSL --retry 5 "$dotfiels_url" | \
+    tar xzf - --strip-component=1 --directory="$dotfiles_dir"
+}
+
+### Main
+
+if [ ! -d "$HOME/dotfiles" ]; then
+  fetch_dotfiles
+fi
+
+echo 'Done!'
