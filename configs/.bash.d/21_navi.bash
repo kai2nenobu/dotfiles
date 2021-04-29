41c3529d593febbdc1c29e98d351c1cbcf492630
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-04-29 12:14:53 +0900

    navi widget bashでbashインテグレーションする
    自作のコードはほぼ必要なく、navi widget bashが出力するコードを使えば
    コマンドラインにスニペットを挿入するのは簡単だった。
    ただし
    
        eval "$(navi widget bash)"
    
    としちゃうとnavi widgetの呼び出しキーバインドが C-g 固定になってしまうので、
    navi widget bashの出力の一部をコピペして、キーバインドだけ変えた。
    もし環境変数とかでキーバインドが変えられるようになったら切り替えましょう。
    
    ref. https://github.com/denisidoro/navi/blob/master/docs/installation.md#installing-the-shell-widget

--- configs/.bash.d/21_navi.bash
+++ configs/.bash.d/21_navi.bash
@@ -5,10 +5,29 @@ if ! _find_command navi; then
   _log "Not found 'navi' in PATH" && return
 fi
 
-## naviで選択したスニペットをコマンドラインに挿入する
-_navi_snippet() {
-  declare l=$(navi --print)
-  READLINE_LINE="$l"
-  READLINE_POINT=${#l}
+## Copy from $(navi widget bash)
+_navi_call() {
+    local result="$(navi "$@" </dev/tty)"
+    if [ -z "${result}" ]; then
+        result="$(navi --print </dev/tty)"
+    fi
+    printf "%s" "$result"
 }
-bind -x '"\C-x\C-n": _navi_snippet'
+
+_navi_widget() {
+    local -r input="${READLINE_LINE}"
+    local -r last_command="$(echo "${input}" | navi fn widget::last_command)"
+
+    if [ -z "${last_command}" ]; then
+        local -r output="$(_navi_call --print --fzf-overrides '--no-select-1')"
+    else
+        local -r find="$last_command"
+        local -r replacement="$(_navi_call --print --query "${last_command}")"
+        local -r output="${input//$find/$replacement}"
+    fi
+
+    READLINE_LINE="$output"
+    READLINE_POINT=${#READLINE_LINE}
+}
+
+bind -x '"\C-x\C-n": _navi_widget'
