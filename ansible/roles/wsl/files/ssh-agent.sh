a4b84b82ed361c71f01703000c9d89095c56eb52
Tsunenobu Kai <kai2nenobu@gmail.com>
2022-05-11 18:43:29 +0900

    shellcheckの指摘を修正する
    CI上はバージョンのおかげか指摘されていないが、手元の最新版(0.8.0)で
    チェックすると指摘されていた。

--- ansible/roles/wsl/files/ssh-agent.sh
+++ ansible/roles/wsl/files/ssh-agent.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=sh
 if [ -d "/run/WSL" ]; then
   # Use npiperelay on WSL2
-  export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+  export SSH_AUTH_SOCK="$HOME/.ssh/agent.sock"
   ss -a | grep -q "$SSH_AUTH_SOCK"
   # shellcheck disable=SC2181
   if [ $? -ne 0 ]; then
@@ -11,5 +11,5 @@ if [ -d "/run/WSL" ]; then
 else
   # Use wsl-ssh-agent on WSL1
   # shellcheck disable=SC2153 # WSL_AUTH_SOCKはWindows側から引き継ぐの問題ない
-  [ -n "$WSL_AUTH_SOCK" ] && export SSH_AUTH_SOCK=$WSL_AUTH_SOCK
+  [ -n "$WSL_AUTH_SOCK" ] && export SSH_AUTH_SOCK="$WSL_AUTH_SOCK"
 fi
