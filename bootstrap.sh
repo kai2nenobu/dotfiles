40f8cdb9db4b0d3ef4fa444554720fa1d18cb8c5
Tsunenobu Kai <kai2nenobu@gmail.com>
2022-05-26 22:57:10 +0900

    devcontainerやcodespacesではない場合ansibleをインストールする
    ansibleをインストールしないとCIが失敗するのを忘れていた。

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,9 @@
 
 HERE=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
 
-#sh "${HERE}/.bootstrap/ansible.sh"
-
 if [ -n "$CODESPACES" ] || [ -n "$REMOTE_CONTAINERS" ]; then
   # shellcheck source=./.bootstrap/container.bash
   source "${HERE}/.bootstrap/container.bash"
+else
+  sh "${HERE}/.bootstrap/ansible.sh"
 fi
