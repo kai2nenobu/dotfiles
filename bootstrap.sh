21f3b12935f37ea7b348f436c4fd5d7685fb0e5d
Tsunenobu Kai <kai2nenobu@gmail.com>
2022-05-26 22:07:13 +0900

    devcontainer環境でも読み込むようにする
    もともとCodespaces用にcodespace.bashを読み込んでいたが、VSCodeのdevcontainer環境でも
    読み込むようにした。なのでcontainer.bashもcontainer.bashに名前を変えました。

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ HERE=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
 
 sh "${HERE}/.bootstrap/ansible.sh"
 
-if [ -n "$CODESPACES" ]; then
-  # shellcheck source=./.bootstrap/codespace.bash
-  source "${HERE}/.bootstrap/codespace.bash"
+if [ -n "$CODESPACES" ] || [ -n "$REMOTE_CONTAINERS" ]; then
+  # shellcheck source=./.bootstrap/container.bash
+  source "${HERE}/.bootstrap/container.bash"
 fi
