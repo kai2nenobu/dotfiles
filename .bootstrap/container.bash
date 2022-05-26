a854c533dc224da30d455f2c56ca8cf50d7a0550
Tsunenobu Kai <kai2nenobu@gmail.com>
2022-05-26 22:28:00 +0900

    deltaをインストールしてみる

--- .bootstrap/container.bash
+++ .bootstrap/container.bash
@@ -1,8 +1,19 @@
-### Bootstrap a codespace environment like GitHub Codespaces
+### Bootstrap a container environment like GitHub Codespaces
 
 # Check environment
 uname -a
 cat /etc/os-release
 
+HERE=$(CDPATH='' cd -- "$(dirname -- "$BASH_SOURCE")" && pwd)
+ID_LIKE=$(grep ID_LIKE /etc/os-release | awk -F= '{print $2}')
+
+if [ "$ID_LIKE" != "debian" ]; then
+  echo "This script doesn't support distributions other than Debian family." >&2
+  return 0
+fi
+
 # Install packages
 sudo apt update && sudo apt install -y less
+
+. "${HERE}/../configs/.bash_aliases"
+install-delta
