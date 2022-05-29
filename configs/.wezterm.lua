070350619d1c56a80c2747184ecc9809fbccff74
Tsunenobu Kai <kai2nenobu@gmail.com>
2022-05-29 22:43:27 +0900

    weztermの設定ファイルを追加する
    クロスプラットフォームなターミナルエミュレーターを使いたかったので、
    weztermにしてみた。Windowsでも使ってみてWindows Terminalと
    比較したいところ。
    
    ref. https://wezfurlong.org/wezterm/index.html

---
+++ configs/.wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm';
+return {
+    -- color_scheme = "JetBrains Darcula",
+    font = wezterm.font("HackGenNerd Console"),
+    font_size = 14.0,
+    enable_scroll_bar = true,
+}
