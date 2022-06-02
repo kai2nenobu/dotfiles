1104a5eca273e0b0c99697ab100f9eb0d0615682
Tsunenobu Kai <kai2nenobu@gmail.com>
2022-06-02 15:26:59 +0900

    もろもろキーバインドを追加する
    コマンドパレットっぽいものやウィンドウの分割をWindows Terminalの
    キーバインドと合わせて設定した。
    
    キー設定の書き方が微妙に難しく、注意が必要なのはShiftキーの扱いである。
    日本語キーボードでCtrl+Shift+5にキーバインドを設定したい場合、
    weztermでは `key="%", mods="CTRL|SHIFT"` と書かないといけないよう。
    つまりkeyにはShiftを押したきに入力する文字、modsにはShiftを含めた修飾キーを
    設定しないといけないようだ。
    
    `key="5", mods="CTRL|SHIFT"` や `key="%", mods="CTRL"` だとキーバインドが
    割り当てられなかった。地味にハマる。

--- configs/.wezterm.lua
+++ configs/.wezterm.lua
@@ -1,11 +1,23 @@
 local wezterm = require 'wezterm';
 
+-- Common settings
 local ret = {
   -- color_scheme = "JetBrains Darcula",
   font = wezterm.font("HackGenNerd Console"),
   font_size = 14.0,
   enable_scroll_bar = true,
+  -- debug_key_events = true,
 }
+
+-- Key bindings
+keys = {
+  -- Command pallete
+  {key="P", mods="CTRL|SHIFT", action=wezterm.action{ShowLauncherArgs={flags="FUZZY|KEY_ASSIGNMENTS"}}},
+  -- Windows Terminal compatibility
+  {key="=", mods="ALT|SHIFT", action=wezterm.action{SplitVertical={}}},
+  {key="+", mods="ALT|SHIFT", action=wezterm.action{SplitHorizontal={}}},
+}
+ret["keys"] = keys
 
 -- Windows specific
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
@@ -17,9 +29,13 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     label = "PowerShell Core",
     args = {"pwsh.exe"},
   })
+  local wsl_command = {"wsl.exe", "--distribution", "main"}
   table.insert(launch_menu, {
     label = "WSL Default",
-    args = {"wsl.exe", "--distribution", "main"}
+    args = wsl_command
+  })
+  table.insert(keys, {
+    key="%", mods="CTRL|SHIFT", action=wezterm.action{SpawnCommandInNewTab={args=wsl_command}},
   })
   table.insert(launch_menu, {
     label = "Command Prompt",
