3317d8bf2720e1fed4e7eb465143690530e1da0e
Tsunenobu Kai <kai2nenobu@gmail.com>
2022-06-02 13:37:28 +0900

    Windows向けの起動メニューを設定する

--- configs/.wezterm.lua
+++ configs/.wezterm.lua
@@ -11,6 +11,25 @@ local ret = {
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   -- Default program
   ret["default_prog"] = {"pwsh.exe"}
+  -- Launch menu
+  local launch_menu = {}
+  table.insert(launch_menu, {
+    label = "PowerShell Core",
+    args = {"pwsh.exe"},
+  })
+  table.insert(launch_menu, {
+    label = "WSL Default",
+    args = {"wsl.exe", "--distribution", "main"}
+  })
+  table.insert(launch_menu, {
+    label = "Command Prompt",
+    args = {"cmd.exe"}
+  })
+  table.insert(launch_menu, {
+    label = "Windows PowerShell",
+    args = {"powershell.exe"},
+  })
+  ret["launch_menu"] = launch_menu
 end
 
 return ret
