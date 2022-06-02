7521860fe148500486fc3f3f965262b038e9245f
Tsunenobu Kai <kai2nenobu@gmail.com>
2022-06-02 13:25:02 +0900

    WindowsではデフォルトプログラムをPowerShell Coreにする

--- configs/.wezterm.lua
+++ configs/.wezterm.lua
@@ -1,7 +1,16 @@
 local wezterm = require 'wezterm';
-return {
-    -- color_scheme = "JetBrains Darcula",
-    font = wezterm.font("HackGenNerd Console"),
-    font_size = 14.0,
-    enable_scroll_bar = true,
+
+local ret = {
+  -- color_scheme = "JetBrains Darcula",
+  font = wezterm.font("HackGenNerd Console"),
+  font_size = 14.0,
+  enable_scroll_bar = true,
 }
+
+-- Windows specific
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  -- Default program
+  ret["default_prog"] = {"pwsh.exe"}
+end
+
+return ret
